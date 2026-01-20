c478ff35fcd01f041865d3f001f3d0f5b154a5fe
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-01-20 05:52:59 +0300

    feat(types): Add types for a todo.txt list item

--- src/vitodo/types.py
+++ src/vitodo/types.py
@@ -1,5 +1,10 @@
+from datetime import date
+from enum import Enum, auto
 from pydantic import BaseModel
 from pathlib import Path
+
+from pydantic.dataclasses import dataclass
+
 
 class GeneralConfig(BaseModel):
     todo_path: Path = Path()
@@ -7,3 +12,22 @@ class GeneralConfig(BaseModel):
 
 class ConfigModel(BaseModel):
     general: GeneralConfig
+
+
+class Priority(Enum):
+    A = auto()
+    B = auto()
+    C = auto()
+    D = auto()
+    E = auto()
+    F = auto()
+
+
+@dataclass
+class TodoItem:
+    priority: Priority
+    start_date: date
+    description: str
+    project: str
+    context: str
+    due_date: date
