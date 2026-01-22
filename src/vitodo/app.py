fa31b891521ed9fd8534d45aec75d5fb32983e88
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-01-22 06:33:25 +0300

    chore(app): Create a testing environment (testing in prod?)

--- src/vitodo/app.py
+++ src/vitodo/app.py
@@ -1,1 +1,21 @@
-app = typer.Typer(help="A very practical and minimal todo.txt CLI tool")
+import typer
+from vitodo.config import config
+from vitodo.parser import Parser
+from vitodo.visual import Visualizer
+
+app = typer.Typer(
+    help="A very practical and minimal todo.txt CLI tool",
+    context_settings={"help_option_names": ["-h", "--help"]},
+)
+
+
+@app.command()
+def dothings():
+    parser = Parser(config.general.clean_description)
+    visualizer = Visualizer(parser.todo_items)
+    visualizer.group_by(config.tables.group_by)
+    visualizer.generate_table()
+
+
+if __name__ == "__main__":
+    app()
